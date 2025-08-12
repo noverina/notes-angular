bf3fec777f747610e59423c736d2ad70b0d60e9e
Noverina Sari <noverina.sari@indocyber.id>
2025-08-12 16:55:54 +0700

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { Note } from './pages/note/note';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Note }];
---
+++ src/app/pages/note/note.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note',
+  imports: [],
+  templateUrl: './note.html',
+  styleUrl: './note.css'
+})
+export class Note {
+
+}
