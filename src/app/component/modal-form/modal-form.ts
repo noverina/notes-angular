832246b8b8010bb284e12509d5cbad6b8a3c02be
Noverina Sari <noverina.sari@indocyber.id>
2025-08-14 09:46:23 +0700

    adjusted form validation

--- src/app/component/modal-form/modal-form.ts
+++ src/app/component/modal-form/modal-form.ts
@@ -28,7 +28,7 @@ export class ModalForm {
     content: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
-      Validators.maxLength(300),
+      Validators.maxLength(500),
     ]),
   });
 
@@ -38,13 +38,18 @@ export class ModalForm {
     if (this.form.valid) {
       const { title, content } = this.form.value;
       const payload: InsertNote = { title: title!, content: content! };
-      this.noteService.insertNote(payload).subscribe((response) => {
-        if (!response.isError) {
-          this.modalService.close();
-          this.refresh.emit();
-        } else {
+      this.noteService.insertNote(payload).subscribe({
+        next: (response) => {
+          if (!response.isError) {
+            this.modalService.close();
+            this.refresh.emit();
+          } else {
+            this.modalService.open({ key: 'error', type: 'error' });
+          }
+        },
+        error: (err) => {
           this.modalService.open({ key: 'error', type: 'error' });
-        }
+        },
       });
     } else {
       this.form.markAllAsTouched();
